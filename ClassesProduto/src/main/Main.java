301db348b2683330d4e9bf020155faf4e82990d3
Ferdinando Rainert <nandorainert2004@gmail.com>
2026-08-13 15:17:28 -0300

    Adicionando projeto de estudo, interação entre classes main e Produto

---
+++ ClassesProduto/src/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.io.Closeable;
+import java.util.Scanner;
+
+import main.model.Produto;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Produto p1 = new Produto();
+		
+		p1.nome = "Notebook";
+		p1.preco = 122.0;
+		p1.desconto = 0.2;
+		
+		var p2 = new Produto();
+		
+		p2.nome = "Celular";
+		p2.preco = 100.5;
+		p2.desconto = 0.1;
+		
+		double precoFinal1 =  p1.preco * (1-p1.desconto);
+		double precoFinal2 =  p2.preco * (1-p2.desconto);
+		
+		double media = (precoFinal1 + precoFinal2)/2;
+		
+		System.out.println("Valores:");
+		System.out.println(p1.nome + " " + precoFinal1);
+		System.out.println(p2.nome + " " + precoFinal2);
+		System.out.println("Média: " + media);
+	}
+}
---
+++ ClassesProduto/src/main/model/Produto.java
@@ -0,0 +1,9 @@
+package main.model;
+
+public class Produto {
+	
+	public String nome;
+	public double preco;
+	public double desconto;
+	
+}
